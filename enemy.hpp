040c1fec1d58ecc9faa0bb545aa6996735f7170d
RILMIX <relisam3@gmail.com>
2025-09-17 12:37:12 +0300

    derection added

---
+++ enemy.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include "Item.hpp"
+
+class enemy
+{
+	string man;
+	int health;
+	int agility;
+	int strength;
+
+};
---
+++ engine.hpp
@@ -0,0 +1,7 @@
+#pragma once
+class engine
+{
+public:
+    void Init();
+    void Run();
+};
